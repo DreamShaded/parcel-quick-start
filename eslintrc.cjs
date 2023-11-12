04c3b9d6e174d7c97324bfdc733a11e21b8c8616
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-11-12 03:21:47 +0300

    feat: initial

---
+++ eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // trailing comma - less git changes!
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+    }],
+    '@typescript-eslint/comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+    }],
+    // always semicolon
+    semi: ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always'],
+    quotes: ['error', 'single'],
+
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+  },
+};
